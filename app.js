8af512c296c506a63e64f103e88b52e9b27d0b9c
JRickyAV <ricky_a_2002@hotmail.com>
2023-11-25 17:53:03 -0600

    Ultima version

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const dgram = require('dgram');
 const server = dgram.createSocket('udp4');
 
@@ -10,6 +11,16 @@ server.on('listening', () => {
 
 server.on('message', (message, remote) => {
   console.log(`Mensaje recibido de ${remote.address}:${remote.port}: ${message}`);
+
+  // Enviar un mensaje de respuesta al cliente
+  const responseMessage = '¡Mensaje recibido con éxito!';
+  server.send(responseMessage, remote.port, remote.address, (err) => {
+    if (err) {
+      console.error(`Error al enviar la respuesta al cliente: ${err}`);
+    } else {
+      console.log(`Respuesta enviada al cliente: ${responseMessage}`);
+    }
+  });
 });
 
 server.bind(PORT);
